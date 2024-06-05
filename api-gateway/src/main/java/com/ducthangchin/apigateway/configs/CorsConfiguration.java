9589f8388c7a58c4a721c8612ff30e890d15b12f
ducthangchin <trinhducthang201@gmail.com>
2024-06-05 19:23:04 +0300

    chore(api-gateway): add cors policy

---
+++ api-gateway/src/main/java/com/ducthangchin/apigateway/configs/CorsConfiguration.java
@@ -0,0 +1,27 @@
+package com.ducthangchin.apigateway.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("*")); // Allow any origin
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST","PUT", "DELETE"));
+        corsConfig.addAllowedHeader("Content-Type");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }}
